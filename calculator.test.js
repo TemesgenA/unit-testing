3386443309cdd88746e644c58703455b6c579afb
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-18 17:51:47 +0300

    revert unnecessary commit

--- calculator.test.js
+++
@@ -1,9 +0,0 @@
-const calculate = require('./calculator.js');
-
-
-describe('check calculator', () => {
-    test('Testing for sum', () => {
-        expect(calculate.add(3, 5)).toBe(8);
-        expect(calculate.add(-3, -5)).toBe(-8);
-    });
-})
