a94bb25fc6e96a27679194a647ebbc5c7739813d
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-18 14:59:58 +0300

    create calculator class

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let calculator = class {
+    constructor(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    };
+    add(num1, num2) {
+        return num1 + num2;
+    };
+
+    subtract(num1, num2) {
+        return num1 - num2;
+    }
+
+    multiply(num1, num2) {
+        return num1 * num2;
+    }
+
+    divide(num1, num2) {
+        return num1 / num2;
+    }
+}
+const calculate = new calculator;
+const sum = calculate.add(-3, -5);
+console.log(sum);
+module.exports = calculator;
