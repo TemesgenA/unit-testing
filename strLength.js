2bae20f5a193e0580858844c285fb9e329e9beac
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-18 11:10:38 +0300

    create stringLength function

---
+++ strLength.js
@@ -0,0 +1,6 @@
+export default function stringLength(string) {
+    for(let i = 0; i < string.length; i++) {
+        return string.length;
+    }
+}
+console.log(stringLength('hello'));
---
+++ strLength.test.js
@@ -0,0 +1,5 @@
+import stringLength from "./strLength";
+
+test('counts the number of characters in a string', (string) => {
+    expect(string.length).tobe
+});
