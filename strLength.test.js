4e548137e80b94a933dbd2ecfe08568da79fb9da
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-18 11:57:20 +0300

    modify strLength.test.js

--- strLength.test.js
+++ strLength.test.js
@@ -2,5 +2,6 @@ let stringLength = require('./strLength.js');
 
 test('counts the number of characters in a string', () => {
     expect(stringLength('hello')).toBe(5);
+    expect(stringLength('Hello World!')).toBe(12);
 }
 );
