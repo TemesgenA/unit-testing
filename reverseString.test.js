28c13ff8b3b45fba86fbe3f1201bf34427c3ad31
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-18 12:36:50 +0300

    modift reverseString function

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,5 +1,5 @@
-const reverseString = require('./reverseString.js');
+let reverseString = require('./reverseString.js');
 
 test('check for reversed string', () => {
     expect(reverseString('Hello').toBe('olleH'));
-})
+});
